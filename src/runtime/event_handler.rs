fbb76de1f13dbdcf3c675079ca88659150679320
Guy Or <guydude1@gmail.com>
2022-06-23 10:53:10 +0300

    rewrite event handler docs

--- src/runtime/event_handler.rs
+++ src/runtime/event_handler.rs
@@ -7,12 +7,19 @@ use crate::data::context::LambdaContext;
 use serde::Serialize;
 use std::fmt::Display;
 
+/// Defines an event handler plus any initilization logic required for implementing the lambda.
+/// This is the main interface users of the library should implement, it accepts 3 type parameters:
+/// `O` - Defines the lambda's output type which must implement or derive [`serde::Serialize`] in order to be sent as a JSON to the RuntimeAPI.
+/// `E` - Defines the lambda's error output type which must implement or derive [`Display`].
+/// `I` - Defines the concrete implementation of the context object used at runtime. Typically [`crate::data::context::EventContext`].
 pub trait EventHandler<O: Serialize, E: Display, I: LambdaContext> {
-    /// The initialization method runs once in order to up any resources that are reusable across the lifetime of the lambda.
-    /// It may fail during runtime (e.g if a db connection was not succesfully opened, etc..) and in that case
-    /// the method returns an Err variant of the same [`E`] type defined for the event handler.
+    /// Sets up any resources that are reusable across the lifetime of the lambda instance.
+    /// Returns a [`Result`] that indicates whether initialization succeeded and if not contains an error object.
     fn initialize(&mut self) -> Result<(), E>;
-    /// Processes each incoming lambda event.
-    /// The [`event`] argument contains the JSON event data as a string slice, which should be deserialized by the implementation.
+    /// Processes each incoming lambda event and returns a [`Result`] with the lambda's output.
+    /// # Arguments
+    ///
+    /// * `event` - The JSON event as a string slice, should be deserialized by the implementation.
+    /// * `context` - A shared reference to the current event context.
     fn on_event(&mut self, event: &str, context: &I) -> Result<O, E>;
 }
